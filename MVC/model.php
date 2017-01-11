c6beb7aeb819ad093e82826ffa62ebf2e9b1b950
vmstr <marcroesner@googlemail.com>
2017-01-11 10:04:24 +0100

    Update model.php
    Code indented

--- MVC/model.php
+++ MVC/model.php
@@ -1,34 +1,34 @@
 <?php 
 	class model { 
-        public function jsonArray($file) { 
-            $input_json = file_get_contents("./data/".$file, FILE_USE_INCLUDE_PATH); 
-			$input_json = utf8_encode($input_json); 
-			$array_output = json_decode($input_json, true); 
-			return $array_output; 
-        }
+		public function jsonArray($file) { 
+		    $input_json = file_get_contents("./data/".$file, FILE_USE_INCLUDE_PATH); 
+				$input_json = utf8_encode($input_json); 
+				$array_output = json_decode($input_json, true); 
+				return $array_output; 
+		}
 		function getArticle() { 
 			return $this->jsonArray("blog.json"); 
 		}  
 		function getNavigation() { 
 			return $this->jsonArray("navigation.json");  
 		} 
-        function getCustompage() { 
+		function getCustompage() { 
 			return $this->jsonArray("page.json");  
 		} 
-        function getConfig() { 
+		function getConfig() { 
 			return $this->jsonArray("config.json");  
 		} 
-        function getPage($page) {
-            $output = file_get_contents('./layout/'.$page.'.php', FILE_USE_INCLUDE_PATH); 
-            return $output; 
-        } 
-        function getCustomcontent() {
-            $output = file_get_contents('./layout/custompage.php', FILE_USE_INCLUDE_PATH); 
-            return $output; 
-        } 
-        function getLayout() { 
-            $layout = file_get_contents('./layout/layout.php', FILE_USE_INCLUDE_PATH); 
-            return $layout; 
-        }
+		function getPage($page) {
+		    $output = file_get_contents('./layout/'.$page.'.php', FILE_USE_INCLUDE_PATH); 
+		    return $output; 
+		} 
+		function getCustomcontent() {
+		    $output = file_get_contents('./layout/custompage.php', FILE_USE_INCLUDE_PATH); 
+		    return $output; 
+		} 
+		function getLayout() { 
+		    $layout = file_get_contents('./layout/layout.php', FILE_USE_INCLUDE_PATH); 
+		    return $layout; 
+		}
 	} 
 ?> 
