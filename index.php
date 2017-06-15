80fc2f12ac683fc2ba7c6a34770e14a401972624
vmaster89 <marcroesner@googlemail.com>
2017-06-15 22:10:33 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 require_once('./MVC/controller.php'); 
 // Layout is defined in layout.php. Model is defined in model.php. Controller is defined in controller.php. 
-class constructor {	
+class main {	
 	function __construct() { 
 		$controller = new controller(); 
 		$controller->createWebpage($_GET, $_POST); 
