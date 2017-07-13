78d1b36f8317ef67bc4ff552d13f69b70f931cad
vmstr <vmaster89@github.com>
2017-07-13 22:50:20 +0200

    commited

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('./MVC/controller.php');
 // Layout is defined in layout.php. Model is defined in model.php. Controller is defined in controller.php. New update.
 class main {	
 	function __construct() { 
-		$controller = new $controller(); 
+		$controller = new controller(); 
 		$controller->createWebpage($_GET, $_POST); 
 	} 
 } 
