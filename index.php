d27eac64b6641f5ff5fdf093e5caf4dfae681d99
vmaster89 <vmaster89@github.com>
2017-07-13 22:46:45 +0200

    vmaster89

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('./MVC/controller.php'); 
-// Layout is defined in layout.php. Model is defined in model.php. Controller is defined in controller.php. 
+// Layout is defined in layout.php. Model is defined in model.php. Controller is defined in controller.php. New update.
 class main {	
 	function __construct() { 
 		$controller = new $controller(); 
